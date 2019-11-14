8b944b3be3358eeaac2b103d91983eab769c8ea5
DavidKocen <dkocen0@gmail.com>
2019-11-14 14:39:26 +0000

    working on adding language detection to remove non-english comments. Sentiment analysis only works with english

--- analysis.py
+++ analysis.py
@@ -1,12 +1,23 @@
 import pandas as pd
 from cleantext import clean
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from langdetect import detect
+
+
+def determine_lang(string):
+    try:
+        return detect(string)
+    except:
+        return 'error'
 
 
 def main():
     """Cleans data and runs sentiment analysis"""
-    df = pd.read_csv('data_uncleaned_100repos.csv', header=None,
+    df = pd.read_csv('data_uncleaned_200repos.csv', header=None,
                       names=['language', 'repo', 'issue_id', 'comment_id', 'comment_body'])
+
+    # Removes all rows that don't have a comment in English (hopefully)
+    df = df[df['comment_body'].apply(lambda x: determine_lang(x)) == 'en']
 
     # Runs basic text cleaning function using cleantext library
     df['comment_body'] = df['comment_body'].apply(lambda x: clean(x, no_line_breaks=True, no_urls=True,
@@ -15,6 +26,9 @@ def main():
 
     analyser = SentimentIntensityAnalyzer()
     df['sentiment'] = [analyser.polarity_scores(str(comment))['compound'] for comment in df['comment_body']]
+
+    df.to_csv('data_cleaned_200repos_withSentiment.csv')
+
 
 if __name__ == '__main__':
     main()
