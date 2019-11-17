72af2920eb831261804721e65b47b7ee17a4eab0
DavidKocen <dkocen0@gmail.com>
2019-11-17 15:11:15 +0000

    basic bar graph. Super ugly right now

--- visualization.py
+++ visualization.py
@@ -1,12 +1,16 @@
 from bokeh.plotting import figure, output_file, show
 import pandas as pd
 
-raw_data = pd.read_csv('data_cleaned_200repos_withSentiment.csv')
+def main():
+    raw_data = pd.read_csv('data_cleaned_200repos_withSentiment.csv')
+    repo_names = raw_data.repo.unique()
+    repo_means = raw_data.groupby('repo', as_index=False)['sentiment'].mean().sort_values(by='sentiment')
 
-means = raw_data.groupby('repo', as_index=False)['sentiment'].mean()
+    p = figure(x_range=repo_means.repo, plot_width=10000)
+    p.vbar(x=repo_means.repo, top=repo_means.sentiment, width=0.9)
+    output_file('test.html')
+    show(p)
 
 
-output_file('test.html')
-p = figure(x_range=means['repo'], title='Average Sentiment across repos')
-p.vbar(x=means['repo'], top=means['sentiment'], width=0.9)
-show(p)
+if __name__ == '__main__':
+    main()
