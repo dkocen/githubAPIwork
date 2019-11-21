28ff4e6f07c4771ff2c828ff9f6d5a0efc4ed528
DavidKocen <dkocen0@gmail.com>
2019-11-21 12:36:48 +0000

    added merging of repo info with comment info collected. Makes one big dataframe. One entry per repo

--- analysis.py
+++ analysis.py
@@ -1,32 +1,40 @@
 import pandas as pd
+import re
 from cleantext import clean
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def main():
     """Cleans data and runs sentiment analysis"""
-    # df = pd.read_csv('data_uncleaned200repos_50issues.csv', header=None,
-    #                   names=['language', 'repo', 'issue_id', 'comment_id', 'comment_body'])
-    #
-    # # Runs basic text cleaning function using cleantext library
-    # df['comment_body'] = df['comment_body'].apply(lambda x: clean(x, no_line_breaks=True, no_urls=True,
-    #                       no_emails=True, no_phone_numbers=True, replace_with_url=' ', replace_with_email=' ',
-    #                       replace_with_phone_number=' '))
-    #
-    # analyser = SentimentIntensityAnalyzer()
-    # df['sentiment'] = [analyser.polarity_scores(str(comment))['compound'] for comment in df['comment_body']]
-    #
-    # df.to_csv('data_cleaned200repos_50issues.csv')
+    df = pd.read_csv('data_uncleaned200repos_50issues.csv', header=None,
+                      names=['language', 'repo', 'issue_id', 'comment_id', 'comment_body'])
 
-    df = pd.read_csv('data_cleaned200repos_50issues.csv')
-    repo_info_df = pd.read_csv('repo_info.csv', header=None, names=['name', 'url', 'created', 'subscribers', 'watchers'])
+    # Runs basic text cleaning function using cleantext library
+    df['comment_body'] = df['comment_body'].apply(lambda x: clean(x, no_line_breaks=True, no_urls=True,
+                          no_emails=True, no_phone_numbers=True, replace_with_url=' ', replace_with_email=' ',
+                          replace_with_phone_number=' '))
 
+    # Run sentiment analysis
+    analyser = SentimentIntensityAnalyzer()
+    df['sentiment'] = [analyser.polarity_scores(str(comment))['compound'] for comment in df['comment_body']]
+
+    # Load in additional repo info
+    repo_info_df = pd.read_csv('repo_info.csv', header=None, names=['repo', 'url', 'created', 'subscribers', 'watchers'])
+    repo_info_df['repo'] = repo_info_df['repo'].apply(lambda x: re.sub(r'.*/', '', x)) # Clean up repo names to match df
+
+    # Create dataframe that has sentiment means per repo
     repos_df = df.groupby(['repo', 'language'], as_index=False)['sentiment'].mean()
     repos_df = repos_df.sort_values(by='sentiment')
-    repos_df['repo'] = [repo[0:30] for repo in repos_df['repo']]
-    additional_data = list()
+
+    # Merge additional repo info df with repo means df
+    entries = list()
     for repo in repos_df['repo']:
-        entry = repo_info_df.loc[repo_info_df['name']]
-        additional_data.append([entry['created'], entry['subscribers'], entry['watchers']])
+        entry = repo_info_df.loc[repo_info_df['repo'] == repo]
+        entries.append([entry.iloc[0][2], entry.iloc[0][3], entry.iloc[0][4]])
+
+    entries_df = pd.DataFrame(entries, columns=['creation_date', 'subscribers', 'watchers'])
+    repos_df = pd.concat([repos_df, entries_df], axis=1)
+
+    repos_df.to_csv('data_cleaned200repos_alldata.csv')
 
 
 if __name__ == '__main__':
